7277370d4115955dff285bc4473509e33f54ac34
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2023-03-12 20:38:20 +0900

    format

--- dagger/dagger.d.ts
+++ dagger/dagger.d.ts
@@ -1,3 +1,3 @@
 import Client from "@dagger.io/dagger";
 
-export type Container = ReturnType<typeof Client.prototype.container>
+export type Container = ReturnType<typeof Client.prototype.container>;
--- dagger/terraform.d.ts
+++ dagger/terraform.d.ts
@@ -1,15 +1,15 @@
 export type PlanResult = {
-    resource_changes: ResourceChange[],
-    output_changes?: OutputChanges
-}
+  resource_changes: ResourceChange[];
+  output_changes?: OutputChanges;
+};
 type ResourceChange = {
-    address: string,
-    change: Change
-}
+  address: string;
+  change: Change;
+};
 type OutputChanges = {
-    [key: string]: Change,
-}
+  [key: string]: Change;
+};
 
 type Change = {
-    actions: ('no-op' | 'create' | 'read' | 'update' | 'delete')[]
-}
+  actions: ("no-op" | "create" | "read" | "update" | "delete")[];
+};
